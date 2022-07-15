dffa30b38a7b3d8a82978db849820463717cea0f
Duje Radicic <duje.radicic.00@fesb.hr>
2022-07-15 19:27:33 +0200

    inserts values into the tables

--- back-end/yacht.sql
+++ back-end/yacht.sql
@@ -18,3 +18,8 @@ CREATE TABLE yacht (
     param_id BIGSERIAL NOT NULL REFERENCES yacht_param (id),
     yacht_name VARCHAR(50)
 );
+
+INSERT INTO yacht_param (id, L, B, T, D, Lbc, Cp, Fn) VALUES (1, 10.06, 4, 1.6, 10000, -2.91, 0.56, 0.2875);
+
+INSERT INTO yacht (id, param_id, yacht_name) VALUES (1, 1, 'standfast1');
+INSERT INTO yacht (id, param_id, yacht_name) VALUES (2, 1, 'standfast2');
