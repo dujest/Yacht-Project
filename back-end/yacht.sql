4a43ca38f7c1478218b950fde165dfec8a4cc4b7
Duje Radicic <duje.radicic.00@fesb.hr>
2022-07-15 19:21:25 +0200

    creates the yacht_param table with the id primary key

--- back-end/yacht.sql
+++ back-end/yacht.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE yacht_data;
 
 \c yacht_data;
+
+CREATE TABLE yacht_param (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    L REAL NOT NULL,
+    B REAL NOT NULL,
+    T REAL NOT NULL,
+    D REAL NOT NULL,
+    Lbc REAL NOT NULL,
+    Cp REAL NOT NULL,
+    Fn REAL NOT NULL
+);
