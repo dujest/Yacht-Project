8f2ebbf226c55546319819440e8e6ad549d1f289
Duje Radicic <duje.radicic.00@fesb.hr>
2022-10-22 16:45:20 +0200

    update the yacht parameteres

--- back-end/yacht.sql
+++ back-end/yacht.sql
@@ -4,13 +4,13 @@ CREATE DATABASE yacht_data;
 
 CREATE TABLE yacht_param (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    L REAL NOT NULL,
-    B REAL NOT NULL,
-    T REAL NOT NULL,
-    D REAL NOT NULL,
-    Lbc REAL NOT NULL,
-    Cp REAL NOT NULL,
-    Fn REAL NOT NULL
+    Length_wl REAL NOT NULL,
+    Beam_wl REAL NOT NULL,
+    Draft REAL NOT NULL,
+    Displacement REAL NOT NULL,
+    Centre_of_Buoyance REAL NOT NULL,
+    Prismatic_Coefficient REAL NOT NULL,
+    Froude_Number REAL NOT NULL
 );
 
 CREATE TABLE yacht (
@@ -19,7 +19,8 @@ CREATE TABLE yacht (
     yacht_name VARCHAR(50)
 );
 
-INSERT INTO yacht_param (id, L, B, T, D, Lbc, Cp, Fn) VALUES (1, 10.06, 4, 1.6, 10000, -2.91, 0.56, 0.2875);
+INSERT INTO yacht_param (id, Length_wl, Beam_wl, Draft, Displacement, Centre_of_Buoyance, Prismatic_Coefficient, Froude_Number) 
+VALUES (1, 10.06, 4, 1.6, 10000, -2.91, 0.56, 0.2875);
 
 INSERT INTO yacht (id, param_id, yacht_name) VALUES (1, 1, 'standfast1');
 INSERT INTO yacht (id, param_id, yacht_name) VALUES (2, 1, 'standfast2');
