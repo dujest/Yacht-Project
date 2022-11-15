3d717f496ee9a2c8700b2de74bcd52ff87721d45
Duje Radicic <duje.radicic.00@fesb.hr>
2022-11-15 19:46:57 +0100

    update yacht_data database

--- back-end/yacht.sql
+++ back-end/yacht.sql
@@ -2,28 +2,15 @@ CREATE DATABASE yacht_data;
 
 \c yacht_data;
 
-CREATE TABLE yacht_param (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    Length_wl REAL NOT NULL,
-    Beam_wl REAL NOT NULL,
-    Draft REAL NOT NULL,
-    Displacement REAL NOT NULL,
-    Centre_of_Buoyance REAL NOT NULL,
-    Prismatic_Coefficient REAL NOT NULL,
-    Froude_Number REAL NOT NULL
-);
-
 CREATE TABLE yacht (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    param_id BIGSERIAL NOT NULL REFERENCES yacht_param (id),
-    yacht_name VARCHAR(50)
+    yacht_name VARCHAR(50) NOT NULL,
+    length_wl REAL NOT NULL,
+    beam_wl REAL NOT NULL,
+    draft REAL NOT NULL,
+    displacement REAL NOT NULL,
+    centre_of_buoyancy REAL NOT NULL,
+    prismatic_coefficient REAL NOT NULL,
+    velocity REAL NOT NULL,
+    resistance REAL
 );
-
-INSERT INTO yacht_param (id, Length_wl, Beam_wl, Draft, Displacement, Centre_of_Buoyance, Prismatic_Coefficient, Froude_Number) 
-VALUES (1, 10.06, 4, 1.6, 10000, -2.91, 0.56, 0.2875);
-
-INSERT INTO yacht (id, param_id, yacht_name) VALUES (1, 1, 'standfast1');
-INSERT INTO yacht (id, param_id, yacht_name) VALUES (2, 1, 'standfast2');
-
-SELECT * FROM yacht 
-JOIN yacht_param ON yacht.param_id = yacht_param.id;
