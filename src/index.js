eaed2d378f084c955fbedd393ee57ffcf6725baf
Duje Radicic <duje.radicic.00@fesb.hr>
2023-02-17 12:12:19 +0100

    add the path module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 require('express-async-errors')
 const app = express()
+const path = require('path')
 const cors = require('cors')
 const routes = require('./routes')
 const errorHandlerMiddleware = require('./errors/error-handler')
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('./client'))
+app.use(express.static(path.join(__dirname, 'client')))
 
 // middleware
 app.use(cors())
